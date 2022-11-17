b65522202283e201faab1842db8de3e214ce94ec
SonyesPlay <andres41600@gmail.com>
2022-11-16 22:09:22 -0500

    se agregan atributos y metodo que calcula el salario neto

--- src/main/java/com/github/sonyesplay/Employee.java
+++ src/main/java/com/github/sonyesplay/Employee.java
@@ -1,5 +1,38 @@
 package com.github.sonyesplay;
 
 public class Employee {
+    private String name;
+    private double salary;
+    private int hours;
 
+    //metodo para calcular el salario neto
+    public double computeNetSalary() {
+        double valueH = salary / 160;
+        double netSalary = valueH * hours;
+        return netSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
 }
--- src/main/java/com/github/sonyesplay/Main.java
+++ src/main/java/com/github/sonyesplay/Main.java
@@ -3,5 +3,11 @@ package com.github.sonyesplay;
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
+        employee.setName("andres");
+        employee.setSalary(1_000_000);
+        employee.setHours(160);
+
+        double netSalary =employee.computeNetSalary();
+        System.out.println(netSalary);
     }
 }
