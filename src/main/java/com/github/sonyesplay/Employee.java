4d8df0c0358d2a42cfa08b0f7ebd32e080ae14bb
SonyesPlay <andres41600@gmail.com>
2022-11-22 02:06:04 -0500

    pruegas de mientras en el main

--- src/main/java/com/github/sonyesplay/Employee.java
+++ src/main/java/com/github/sonyesplay/Employee.java
@@ -3,18 +3,34 @@ package com.github.sonyesplay;
 import java.util.Objects;
 
 public class Employee {
+    public static int NUMBER_EMPLOYEE = 0;
+
+    public int getNumber() {
+        return number;
+    }
+
+    private final int number;
     private final String name;
     private final double salary;
-
     private final int hours;
+    private static final int HOURS_OF_MONTH = 160;
+    private static final double RETENTION_OF_EMPLOYEE = 0.5;
+    private static final double SALARY_TOP = 7_000_000;
+    private static final double SALARY_BOT = 1_000_000;
+    private static final double AUX_TRANSPORT = 65_000;
+   /* private
+    public
+    protected
+    default*/
 
     public Employee(String name, double salary, int hours) {
         valitation(name, salary, hours);
+        NUMBER_EMPLOYEE++;//esto es para contar los empleados se incia en cero y ba agragando 1 + 1
+        this.number = NUMBER_EMPLOYEE;
         this.name = name;
         this.salary = salary;
         this.hours = hours;
     }
-
     //metodo para validar la entrada de los datos
     private void valitation(String name, double salary, double hours) {
         Objects.requireNonNull(name, "El nombre no puede ser nulo");
@@ -28,38 +44,30 @@ public class Employee {
 
     //metodo para calcular el salario neto
     public double valueHour() {
-        return salary / 160;
+        return salary / HOURS_OF_MONTH;
     }
 
     public double computeNetSalary() {
-        double netSalary;
-        netSalary = valueHour() * hours;
-        return netSalary;
+        return valueHour() * hours;
     }
 
     //metodo para verificar si debe tener auxilio de transporte
     public boolean auxTransport() {
-        boolean aux;
-        aux = salary <= 1_000_000;
-
-        return aux;
+        return salary <= SALARY_BOT;
     }
 
     //metodo para verificar si debe tener a retencion en la fuente
     public boolean rentention() {
-        boolean aux;
-        aux = salary > 7_000_000;
-
-        return aux;
+        return salary > SALARY_TOP;
     }
 
     //metodo para validar cual es el salario total
     public double salaryTotal() {
         double salaryT;
         if (auxTransport()) {
-            salaryT = computeNetSalary() + 65000;
+            salaryT = computeNetSalary() + AUX_TRANSPORT;
         } else if (rentention()) {
-            salaryT = computeNetSalary() - (salary * 0.05);
+            salaryT = computeNetSalary() - (salary * RETENTION_OF_EMPLOYEE);
         } else {
             salaryT = computeNetSalary();
         }
@@ -72,15 +80,16 @@ public class Employee {
 
         builder.append("DESPRENDIBLE DE PAGO\n");
         builder.append("_________________________________________________\n");
+        builder.append("Este trabajador es el numero: ").append(number).append("\n");
         builder.append("Nombre: ").append(name).append("\n");
         builder.append("Salario: ").append(salary).append("\n");
         builder.append("Horas trabajadas: ").append(hours).append("\n");
         builder.append("Valor hora: ").append(valueHour()).append("\n");
         if (auxTransport()) {
-            builder.append("Auxilio de transporte: ").append(65000).append("\n");
+            builder.append("Auxilio de transporte: ").append(AUX_TRANSPORT).append("\n");
         } else builder.append("Auxilio de transporte: ").append("no tiene auxilio").append("\n");
         if (rentention()) {
-            builder.append("Retención: ").append((salary * 0.05)).append("\n");
+            builder.append("Retención: ").append((salary * RETENTION_OF_EMPLOYEE)).append("\n");
         } else builder.append("Auxilio de transporte: ").append("no tiene retencion").append("\n");
         builder.append("Total a pagar: ").append(salaryTotal()).append("\n");
         builder.append("_________________________________________________\n");
--- src/main/java/com/github/sonyesplay/Main.java
+++ src/main/java/com/github/sonyesplay/Main.java
@@ -1,12 +1,30 @@
 package com.github.sonyesplay;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee("andres banguero", 1_000_000, 160);
-        Employee employee1 = new Employee("cristo bbb", 8_000_000, 160);
-        Employee employee2 = new Employee("pedro zambrano", 1_500_000, 160);
-        System.out.println(employee);
-        System.out.println(employee1);
-        System.out.println(employee2);
+        Scanner sc = new Scanner(System.in);
+        boolean x = true;
+        while (x) {
+            System.out.println("A continuacion ingrese el nombre");
+            String name = sc.next();
+            System.out.println("A continuacion ingrese el salario");
+            double salary = sc.nextDouble();
+            System.out.println("A continuacion ingrese el horas laboradas");
+            int hours = sc.nextInt();
+            Employee employee = new Employee(name, salary, hours);
+            System.out.println(employee);
+            System.out.println("si desea averiguar el salario de otro trabajador Presionar 1 de lo contrario presione cualquier letra");
+            int valitate = sc.nextInt();
+            if (valitate == 1) {
+            } else {
+                x = false;
+                System.out.println("se cerro el program");
+            }
+
+
+
+        }
     }
 }
